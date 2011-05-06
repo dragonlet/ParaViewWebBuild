9bc40d4487683cc05ad5fbd748e9b7246ce1331a
KTK <ktk227@gmail.com>
2011-05-06 15:50:04 -0400

    server side plugins

--- pwservice/scripts.cmake
+++ pwservice/scripts.cmake
@@ -2,8 +2,20 @@
 
 set(PVWEB_HOME ${INSTALL_DIR})
 
+
 configure_file( run_pw.sh.in
                 ${INSTALL_DIR}/run_pw.sh @ONLY)
 
 configure_file( use_pvweb.sh.in
                 ${INSTALL_DIR}/use_pvweb.sh @ONLY)
+
+file(COPY findPort.sh manta-init.py DESTINATION ${INSTALL_DIR}/bin)
+
+configure_file( manta.sh.in
+                ${INSTALL_DIR}/bin/manta.sh @ONLY)
+                
+configure_file( MantaLoader.py.in
+                ${WORKING_DIR}/plugins/MantaLoader.py @ONLY)
+
+configure_file( Calc.py
+                ${WORKING_DIR}/plugins/Calc.py COPYONLY)
