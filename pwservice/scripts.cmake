1521e33d494d8751ca114b1e980f9d84609ead13
KTK <ktk227@gmail.com>
2011-10-19 12:56:10 -0400

    Added some new script to start/stop X server

--- pwservice/scripts.cmake
+++ pwservice/scripts.cmake
@@ -2,25 +2,30 @@
 
 set(PVWEB_HOME ${INSTALL_DIR})
 
+# main stripts
+foreach (cmd_file start_pw.sh
+                  stop_pw.sh
+                  use_pvweb.sh
+        )
+    configure_file( ${cmd_file}.in
+                    ${INSTALL_DIR}/${cmd_file} @ONLY)
+endforeach()
 
-configure_file( run_pw.sh.in
-                ${INSTALL_DIR}/run_pw.sh @ONLY)
-
-configure_file( use_pvweb.sh.in
-                ${INSTALL_DIR}/use_pvweb.sh @ONLY)
-
-file(COPY findPort.sh manta-init.py DESTINATION ${INSTALL_DIR}/bin)
-
+# bin scripts
 foreach (cmd_file manta.sh
                   PWServer.sh
                   pw_default.sh
                   pw_parallel.sh
                   pw_pvserver.sh
                   cse_pvserver.sh
+                  start_X.sh
+                  stop_X.sh
         )          
     configure_file( ${cmd_file}.in
                     ${INSTALL_DIR}/bin/${cmd_file} @ONLY)
 endforeach()
+
+file(COPY findPort.sh manta-init.py DESTINATION ${INSTALL_DIR}/bin)
 
 configure_file( plugins/MantaLoader.py.in
                 ${WORKING_DIR}/plugins/MantaLoader.py @ONLY)
